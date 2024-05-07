5dab7734813e5dcf0ff4495fd10353a6400d4db1
gao <138052825@qq.com>
2024-05-07 18:43:11 +0800

    change build ci

---
+++ api/migrations/versions/b49b7213db1a_.py
@@ -0,0 +1,25 @@
+"""empty message
+
+Revision ID: b49b7213db1a
+Revises: 3c7cac9521c6, 03b5a423b43d
+Create Date: 2024-05-07 09:34:10.849157
+
+"""
+from alembic import op
+import models as models
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b49b7213db1a'
+down_revision = ('3c7cac9521c6', '03b5a423b43d')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
