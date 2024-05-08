a8823d8113fcbae2eb5ec185c46f87fba687ae36
wintercomming <1156032282@qq.com>
2024-05-08 12:11:35 +0800

    feat: 去掉新增卡片角色校验

--- web/app/(commonLayout)/apps/Apps.tsx
+++ web/app/(commonLayout)/apps/Apps.tsx
@@ -9,7 +9,7 @@ import NewAppCard from './NewAppCard'
 import useAppsQueryState from './hooks/useAppsQueryState'
 import type { AppListResponse } from '@/models/app'
 import { fetchAppList } from '@/service/apps'
-import { useAppContext } from '@/context/app-context'
+// import { useAppContext } from "@/context/app-context";
 import { NEED_REFRESH_APP_LIST_KEY } from '@/config'
 import { CheckModal } from '@/hooks/use-pay'
 import TabSliderNew from '@/app/components/base/tab-slider-new'
@@ -33,12 +33,14 @@ const getKey = (
   keywords: string,
 ) => {
   if (!pageIndex || previousPageData.has_more) {
-    const params: any = { url: 'apps', params: { page: pageIndex + 1, limit: 30, name: keywords } }
+    const params: any = {
+      url: 'apps',
+      params: { page: pageIndex + 1, limit: 30, name: keywords },
+    }
 
     if (activeTab !== 'all')
       params.params.mode = activeTab
-    else
-      delete params.params.mode
+    else delete params.params.mode
 
     if (tags.length)
       params.params.tag_ids = tags
@@ -50,33 +52,59 @@ const getKey = (
 
 const Apps = () => {
   const { t } = useTranslation()
-  const { isCurrentWorkspaceManager } = useAppContext()
+  // const { isCurrentWorkspaceManager } = useAppContext();
   const showTagManagementModal = useTagStore(s => s.showTagManagementModal)
   const [activeTab, setActiveTab] = useTabSearchParams({
     defaultTab: 'all',
   })
-  const { query: { tagIDs = [], keywords = '' }, setQuery } = useAppsQueryState()
+  const {
+    query: { tagIDs = [], keywords = '' },
+    setQuery,
+  } = useAppsQueryState()
   const [tagFilterValue, setTagFilterValue] = useState<string[]>(tagIDs)
   const [searchKeywords, setSearchKeywords] = useState(keywords)
-  const setKeywords = useCallback((keywords: string) => {
-    setQuery(prev => ({ ...prev, keywords }))
-  }, [setQuery])
-  const setTagIDs = useCallback((tagIDs: string[]) => {
-    setQuery(prev => ({ ...prev, tagIDs }))
-  }, [setQuery])
+  const setKeywords = useCallback(
+    (keywords: string) => {
+      setQuery(prev => ({ ...prev, keywords }))
+    },
+    [setQuery],
+  )
+  const setTagIDs = useCallback(
+    (tagIDs: string[]) => {
+      setQuery(prev => ({ ...prev, tagIDs }))
+    },
+    [setQuery],
+  )
 
   const { data, isLoading, setSize, mutate } = useSWRInfinite(
-    (pageIndex: number, previousPageData: AppListResponse) => getKey(pageIndex, previousPageData, activeTab, tagIDs, searchKeywords),
+    (pageIndex: number, previousPageData: AppListResponse) =>
+      getKey(pageIndex, previousPageData, activeTab, tagIDs, searchKeywords),
     fetchAppList,
     { revalidateFirstPage: true },
   )
 
   const anchorRef = useRef<HTMLDivElement>(null)
   const options = [
-    { value: 'all', text: t('app.types.all'), icon: <DotsGrid className='w-[14px] h-[14px] mr-1'/> },
-    { value: 'chat', text: t('app.types.chatbot'), icon: <ChatBot className='w-[14px] h-[14px] mr-1'/> },
-    { value: 'agent-chat', text: t('app.types.agent'), icon: <CuteRobot className='w-[14px] h-[14px] mr-1'/> },
-    { value: 'workflow', text: t('app.types.workflow'), icon: <Route className='w-[14px] h-[14px] mr-1'/> },
+    {
+      value: 'all',
+      text: t('app.types.all'),
+      icon: <DotsGrid className="w-[14px] h-[14px] mr-1" />,
+    },
+    {
+      value: 'chat',
+      text: t('app.types.chatbot'),
+      icon: <ChatBot className="w-[14px] h-[14px] mr-1" />,
+    },
+    {
+      value: 'agent-chat',
+      text: t('app.types.agent'),
+      icon: <CuteRobot className="w-[14px] h-[14px] mr-1" />,
+    },
+    {
+      value: 'workflow',
+      text: t('app.types.workflow'),
+      icon: <Route className="w-[14px] h-[14px] mr-1" />,
+    },
   ]
 
   useEffect(() => {
@@ -91,26 +119,35 @@ const Apps = () => {
   useEffect(() => {
     let observer: IntersectionObserver | undefined
     if (anchorRef.current) {
-      observer = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting && !isLoading && hasMore)
-          setSize((size: number) => size + 1)
-      }, { rootMargin: '100px' })
+      observer = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting && !isLoading && hasMore)
+            setSize((size: number) => size + 1)
+        },
+        { rootMargin: '100px' },
+      )
       observer.observe(anchorRef.current)
     }
     return () => observer?.disconnect()
   }, [isLoading, setSize, anchorRef, mutate, hasMore])
 
-  const { run: handleSearch } = useDebounceFn(() => {
-    setSearchKeywords(keywords)
-  }, { wait: 500 })
+  const { run: handleSearch } = useDebounceFn(
+    () => {
+      setSearchKeywords(keywords)
+    },
+    { wait: 500 },
+  )
   const handleKeywordsChange = (value: string) => {
     setKeywords(value)
     handleSearch()
   }
 
-  const { run: handleTagsUpdate } = useDebounceFn(() => {
-    setTagIDs(tagFilterValue)
-  }, { wait: 500 })
+  const { run: handleTagsUpdate } = useDebounceFn(
+    () => {
+      setTagIDs(tagFilterValue)
+    },
+    { wait: 500 },
+  )
   const handleTagsChange = (value: string[]) => {
     setTagFilterValue(value)
     handleTagsUpdate()
@@ -118,28 +155,41 @@ const Apps = () => {
 
   return (
     <>
-      <div className='sticky top-0 flex justify-between items-center pt-4 px-12 pb-2 leading-[56px] bg-gray-100 z-10 flex-wrap gap-y-2'>
+      <div className="sticky top-0 flex justify-between items-center pt-4 px-12 pb-2 leading-[56px] bg-gray-100 z-10 flex-wrap gap-y-2">
         <TabSliderNew
           value={activeTab}
           onChange={setActiveTab}
           options={options}
         />
-        <div className='flex items-center gap-2'>
-          <TagFilter type='app' value={tagFilterValue} onChange={handleTagsChange} />
-          <SearchInput className='w-[200px]' value={keywords} onChange={handleKeywordsChange} />
+        <div className="flex items-center gap-2">
+          <TagFilter
+            type="app"
+            value={tagFilterValue}
+            onChange={handleTagsChange}
+          />
+          <SearchInput
+            className="w-[200px]"
+            value={keywords}
+            onChange={handleKeywordsChange}
+          />
         </div>
       </div>
-      <nav className='grid content-start grid-cols-1 gap-4 px-12 pt-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grow shrink-0'>
-        {isCurrentWorkspaceManager
-          && <NewAppCard onSuccess={mutate} />}
-        {data?.map(({ data: apps }: any) => apps.map((app: any) => (
-          <AppCard key={app.id} app={app} onRefresh={mutate} />
-        )))}
+      <nav className="grid content-start grid-cols-1 gap-4 px-12 pt-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grow shrink-0">
+        {/* {isCurrentWorkspaceManager
+          &&  */}
+        <NewAppCard onSuccess={mutate} />
+        {data?.map(({ data: apps }: any) =>
+          apps.map((app: any) => (
+            <AppCard key={app.id} app={app} onRefresh={mutate} />
+          )),
+        )}
         <CheckModal />
       </nav>
-      <div ref={anchorRef} className='h-0'> </div>
+      <div ref={anchorRef} className="h-0">
+        {' '}
+      </div>
       {showTagManagementModal && (
-        <TagManagementModal type='app' show={showTagManagementModal} />
+        <TagManagementModal type="app" show={showTagManagementModal} />
       )}
     </>
   )
