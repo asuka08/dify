c2aba11e881e08a8b6d9a32e8e124069b12ac061
gao <138052825@qq.com>
2024-06-07 12:08:34 +0800

    merge migration

---
+++ api/migrations/versions/543bfdcb0e1f_merge.py
@@ -0,0 +1,22 @@
+"""merge
+
+Revision ID: 543bfdcb0e1f
+Revises: 4e99a8df00ff, ca5927e743c9
+Create Date: 2024-06-07 04:04:01.647437
+
+"""
+import models as models
+
+# revision identifiers, used by Alembic.
+revision = '543bfdcb0e1f'
+down_revision = ('4e99a8df00ff', 'ca5927e743c9')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
