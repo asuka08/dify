c2a7df67b6f3f2734140a289c02a13a6121c8033
gao <138052825@qq.com>
2024-06-07 20:05:38 +0800

    change result time out

--- web/app/components/share/text-generation/result/index.tsx
+++ web/app/components/share/text-generation/result/index.tsx
@@ -183,7 +183,7 @@ const Result: FC<IResultProps> = ({
     let isEnd = false
     let isTimeout = false;
     (async () => {
-      await sleep(1000 * 60) // 1min timeout
+      await sleep(1000 * 3600) // 10min timeout
       if (!isEnd) {
         setRespondingFalse()
         onCompleted(getCompletionRes(), taskId, false)
