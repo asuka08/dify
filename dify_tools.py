e2cd45e9acccad4d8ba990b74ed21b4d56710c27
gao <138052825@qq.com>
2024-07-03 20:47:16 +0800

    增加导入脚本

--- dify_tools.py
+++ dify_tools.py
@@ -4,6 +4,7 @@ from typing_extensions import Annotated
 from rich import print
 from pathlib import Path
 import urllib.parse
+import yaml
 
 app = typer.Typer()
 tool_app = typer.Typer()
@@ -35,7 +36,7 @@ to_auth_header = {"Authorization": f"Bearer {to_token}"}
 
 
 @tool_app.command("1", help="迁移工具")
-def compute_kos_red_note_public_data(
+def import_tool(
         tool_name: Annotated[str, typer.Argument(help="工具名称")]
 ):
     from_tool_data = requests.get(
@@ -62,7 +63,7 @@ def compute_kos_red_note_public_data(
 
 # python dify_tools.py app 1 f22246d6-3c1b-47dd-a56a-b5ba05ab1c05 6f2f13c5-06e1-4f99-b0dc-738b1fb7d35a
 @app_app.command("1", help="迁移应用")
-def compute_kos_red_note_public_data(
+def import_app(
         app_id: Annotated[str, typer.Argument(help="应用id")],
         to_app_id: Annotated[str, typer.Argument(help="导入应用id")] = None
 ):
@@ -91,6 +92,33 @@ def compute_kos_red_note_public_data(
             f"{to_base}/console/api/apps/{to_app_id}/workflows/publish",
             headers=to_auth_header).json()
         print(result)
+
+
+@app_app.command("2", help="导入新app")
+def import_new_app(
+        app_id: Annotated[str, typer.Argument(help="应用id")]
+):
+    from_app = requests.get(f"{from_base}/console/api/apps/{app_id}/export", headers=from_auth_header).json()["data"]
+
+    # 读取 YAML 文件
+    config = yaml.safe_load(from_app)
+    # 打印读取到的内容
+    to_tool_providers = requests.get(f"{to_base}/console/api/workspaces/current/tool-providers",
+                                     headers=to_auth_header).json()
+    tool_dict = dict((f"{provider['type']}_{provider['name']}", provider["id"]) for provider in to_tool_providers)
+    if config["app"]["mode"] in ["workflow", "advanced-chat"]:
+        for node in config["workflow"]["graph"]["nodes"]:
+            if node["data"]["type"] == "tool":
+                if node["data"]["provider_type"] in ["api", "workflow"]:
+                    node["data"]["provider_id"] = tool_dict[
+                        f'{node["data"]["provider_type"]}_{node["data"]["provider_name"]}']
+
+    config = yaml.dump(config)
+
+    result = requests.post(
+        f"{to_base}/console/api/apps/import",
+        headers=to_auth_header, json={"data": config}).json()
+    print(result)
 
 
 if __name__ == '__main__':
