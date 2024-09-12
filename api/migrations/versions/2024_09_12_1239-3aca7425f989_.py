db093c9bc06ca33b86c08a3b6bc0fa8e200c254c
gao <138052825@qq.com>
2024-09-12 20:49:11 +0800

    add db

---
+++ api/migrations/versions/2024_09_12_1239-3aca7425f989_.py
@@ -0,0 +1,25 @@
+"""empty message
+
+Revision ID: 3aca7425f989
+Revises: 07a4565e67b6, 675b5321501b
+Create Date: 2024-09-12 12:39:53.909786
+
+"""
+from alembic import op
+import models as models
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3aca7425f989'
+down_revision = ('07a4565e67b6', '675b5321501b')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
