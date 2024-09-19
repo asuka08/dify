e05c281bec232859025fefdd6d4d62538eb49f99
gao <138052825@qq.com>
2024-09-19 19:37:26 +0800

    change limit

--- api/core/app/app_config/features/file_upload/manager.py
+++ api/core/app/app_config/features/file_upload/manager.py
@@ -49,9 +49,6 @@ class FileUploadConfigManager:
 
         if config["file_upload"]["image"]["enabled"]:
             number_limits = config["file_upload"]["image"]["number_limits"]
-            if number_limits < 1 or number_limits > 6:
-                raise ValueError("number_limits must be in [1, 6]")
-
             if is_vision:
                 detail = config["file_upload"]["image"]["detail"]
                 if detail not in {"high", "low"}:
