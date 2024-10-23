27143877318390cfe7af86819294109ba8b8a3e9
gao <138052825@qq.com>
2024-10-23 16:59:59 +0800

    merge db

---
+++ api/migrations/versions/2024_10_23_0856-a4dc72750b80_.py
@@ -0,0 +1,25 @@
+"""empty message
+
+Revision ID: a4dc72750b80
+Revises: 3aca7425f989, bbadea11becb
+Create Date: 2024-10-23 08:56:43.730432
+
+"""
+from alembic import op
+import models as models
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a4dc72750b80'
+down_revision = ('3aca7425f989', 'bbadea11becb')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
